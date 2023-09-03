ee7a56713c02d5e157efd94cdf0a9fa04a0db9a7
Rin-t <rin.t.4921@gmail.com>
2023-09-03 15:29:08 +0900

    Initial Commit

---
+++ SwiftDataTutorial/SwiftDataTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataTutorialApp.swift
+//  SwiftDataTutorial
+//
+//  Created by Rin on 2023/09/03.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
