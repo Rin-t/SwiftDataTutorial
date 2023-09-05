802fa639d767da560e65c2979917788e4ce3b8c6
Rin-t <rin.t.4921@gmail.com>
2023-09-05 20:31:52 +0900

    first commit

--- SwiftDataTutorial/ContentView.swift
+++ SwiftDataTutorial/ContentView.swift
@@ -6,19 +6,99 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+
+    @Environment(\.modelContext) var context
+
+    @State private var showCreate = false
+    @State private var toDoToEdit: ToDoItem?
+
+    @Query(filter: #Predicate<ToDoItem> { $0.isCompleted == true }) private var items: [ToDoItem]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(items) { item in
+                    HStack {
+                        VStack {
+                            if item.isCritical {
+                                Image(systemName: "exclamationmark.3")
+                                    .symbolVariant(.fill)
+                                    .foregroundStyle(.red)
+                                    .font(.largeTitle)
+                                    .bold()
+                            }
+
+                            Text(item.title)
+                                .font(.largeTitle)
+                                .bold()
+
+                            Text("\(item.timeStamp, format: Date.FormatStyle(date: .numeric, time: .shortened))")
+                                .font(.callout)
+                        }
+
+                        Spacer()
+
+                        Button {
+                            withAnimation {
+                                item.isCompleted.toggle()
+                            }
+                        } label: {
+                            Image(systemName: "checkmark")
+                                .symbolVariant(.circle.fill)
+                                .foregroundStyle(item.isCompleted ? .green : .gray)
+                                .font(.largeTitle)
+                        }
+                        .buttonStyle(.plain)
+                    }
+                    .swipeActions {
+                        Button(role: .destructive) {
+                            withAnimation {
+                                context.delete(item)
+                            }
+                        } label: {
+                            Label("Delete", systemImage: "trash")
+                                .symbolVariant(.fill)
+                        }
+
+                        Button {
+                            toDoToEdit = item
+                        } label: {
+                            Label("Edit", systemImage: "pencil")
+                                .tint(.orange)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("My To Do List")
+            .toolbar {
+                ToolbarItem {
+                    Button(action: {
+                        showCreate.toggle()
+                    }, label: {
+                        Label("Add item", systemImage: "plus")
+                    })
+                }
+            }
+            .sheet(isPresented: $showCreate,
+                   content: {
+                NavigationStack {
+                    CreateTodoView()
+                }
+                .presentationDetents([.medium])
+            })
+            .sheet(item: $toDoToEdit) {
+                toDoToEdit = nil
+            } content: { item in
+                UpdateToDoView(item: item)
+            }
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
-}
+//#Preview {
+//    ContentView()
+//        .modelContainer(for: ToDoItem.self)
+//}
